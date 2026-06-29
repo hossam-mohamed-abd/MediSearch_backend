550b35df1285b73fabe62a49059d9b9d0131b811
Hossam Mohamed <hossam545mohamed@gmail.com>
2026-06-29 12:39:52 +0300

    d

--- dist/src/utils/bigintSerializer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serializeBigInt = void 0;
-const serializeBigInt = (data) => {
-    return JSON.parse(JSON.stringify(data, (_, value) => typeof value === 'bigint'
-        ? value.toString()
-        : value));
-};
-exports.serializeBigInt = serializeBigInt;
